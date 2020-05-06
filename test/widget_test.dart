f1c154dbfd9affb3e2dccbf8a3a6be8b046ac420
Remi Rousselet <darky12s@gmail.com>
2020-05-06 12:04:03 +0100

    remove banner in goldens

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1182,6 +1182,7 @@ class Boilerplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: child,
       ),
