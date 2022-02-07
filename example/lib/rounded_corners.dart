ab00951305fd7bd8139554dde748106154983aa3
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-02-07 15:57:23 +0000

    Adapt example for use case

--- example/lib/rounded_corners.dart
+++ example/lib/rounded_corners.dart
@@ -14,10 +14,9 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Example'),
           ),
-          body: Container(
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.centerLeft,
-            child: const RoundedCornersExample(),
+          body: const Padding(
+            padding: EdgeInsets.all(16),
+            child: RoundedCornersExample(),
           ),
         ),
       ),
@@ -37,40 +36,21 @@ class _RoundedCornersExampleState extends State<RoundedCornersExample> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: _ModalEntry(
-        visible: _showPopup,
-        onClose: () => setState(() => _showPopup = false),
-        childAnchor: Alignment.topRight,
-        menuAnchor: Alignment.topLeft,
-        menu: const _Popup(
-          children: [
-            PopupMenuItem<void>(
-              height: 42,
-              child: Text('first'),
+    return _ModalEntry(
+      visible: _showPopup,
+      onClose: () => setState(() => _showPopup = false),
+      popup: _Popup(
+        children: [
+          for (var i = 0; i < 12; i++)
+            ListTile(
+              onTap: () => setState(() => _showPopup = false),
+              title: Text('$i'),
             ),
-            PopupMenuItem<void>(
-              height: 42,
-              child: Text('second'),
-            ),
-            PopupMenuItem<void>(
-              height: 42,
-              child: Text('third'),
-            ),
-            PopupMenuItem<void>(
-              height: 42,
-              child: Text('forth'),
-            ),
-            PopupMenuItem<void>(
-              height: 42,
-              child: Text('fifth'),
-            ),
-          ],
-        ),
-        child: ElevatedButton(
-          onPressed: () => setState(() => _showPopup = true),
-          child: const Text('show popup'),
-        ),
+        ],
+      ),
+      child: ElevatedButton(
+        onPressed: () => setState(() => _showPopup = true),
+        child: const Text('show popup'),
       ),
     );
   }
@@ -102,19 +82,15 @@ class _ModalEntry extends StatelessWidget {
   const _ModalEntry({
     Key? key,
     required this.onClose,
-    required this.menu,
+    required this.popup,
     required this.visible,
-    required this.menuAnchor,
-    required this.childAnchor,
     required this.child,
   }) : super(key: key);
 
   final VoidCallback onClose;
-  final Widget menu;
+  final Widget popup;
   final bool visible;
   final Widget child;
-  final Alignment menuAnchor;
-  final Alignment childAnchor;
 
   @override
   Widget build(BuildContext context) {
@@ -123,16 +99,11 @@ class _ModalEntry extends StatelessWidget {
       onTap: visible ? onClose : null,
       child: PortalTarget(
         visible: visible,
-        portalFollower: menu,
+        portalFollower: popup,
         anchor: const Aligned(
           follower: Alignment.topLeft,
           target: Alignment.bottomLeft,
           widthFactor: 1,
-          backup: Aligned(
-            follower: Alignment.bottomLeft,
-            target: Alignment.topLeft,
-            widthFactor: 1,
-          ),
         ),
         child: IgnorePointer(
           ignoring: visible,
