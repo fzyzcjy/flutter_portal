71d286e68ccd7e70e13f46d376d2bf6cc789ce06
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-02-07 15:48:07 +0000

    Add rounded corners example

---
+++ example/lib/rounded_corners.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_portal/flutter_portal.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Portal(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Example'),
+          ),
+          body: Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            child: const ContextualMenuExample(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContextualMenuExample extends StatefulWidget {
+  const ContextualMenuExample({Key? key}) : super(key: key);
+
+  @override
+  _ContextualMenuExampleState createState() => _ContextualMenuExampleState();
+}
+
+class _ContextualMenuExampleState extends State<ContextualMenuExample> {
+  bool _showMenu = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: _ModalEntry(
+        visible: _showMenu,
+        onClose: () => setState(() => _showMenu = false),
+        childAnchor: Alignment.topRight,
+        menuAnchor: Alignment.topLeft,
+        menu: const Menu(
+          children: [
+            PopupMenuItem<void>(
+              height: 42,
+              child: Text('first'),
+            ),
+            PopupMenuItem<void>(
+              height: 42,
+              child: Text('second'),
+            ),
+          ],
+        ),
+        child: ElevatedButton(
+          onPressed: () => setState(() => _showMenu = true),
+          child: const Text('show menu'),
+        ),
+      ),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  const Menu({
+    Key? key,
+    required this.children,
+  }) : super(key: key);
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      child: IntrinsicWidth(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: children,
+        ),
+      ),
+    );
+  }
+}
+
+class _ModalEntry extends StatelessWidget {
+  const _ModalEntry({
+    Key? key,
+    required this.onClose,
+    required this.menu,
+    required this.visible,
+    required this.menuAnchor,
+    required this.childAnchor,
+    required this.child,
+  }) : super(key: key);
+
+  final VoidCallback onClose;
+  final Widget menu;
+  final bool visible;
+  final Widget child;
+  final Alignment menuAnchor;
+  final Alignment childAnchor;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: visible ? onClose : null,
+      child: PortalTarget(
+        visible: visible,
+        portalFollower: menu,
+        anchor: const Aligned(
+          follower: Alignment.topLeft,
+          target: Alignment.bottomLeft,
+          widthFactor: 1,
+          backup: Aligned(
+            follower: Alignment.bottomLeft,
+            target: Alignment.topLeft,
+            widthFactor: 1,
+          ),
+        ),
+        child: IgnorePointer(
+          ignoring: visible,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
