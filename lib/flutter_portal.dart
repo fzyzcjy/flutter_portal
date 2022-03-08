cd90d5bb8838b4af52dce133c480e9d89531cad1
fzyzcjy <ch271828n@outlook.com>
2022-03-08 09:04:34 +0800

    export identifier

--- lib/flutter_portal.dart
+++ lib/flutter_portal.dart
@@ -1,4 +1,5 @@
 export 'package:flutter_portal/src/anchor.dart' show Anchor, Aligned;
-export 'package:flutter_portal/src/portal.dart' show Portal;
+export 'package:flutter_portal/src/portal.dart'
+    show Portal, PortalIdentifier, PortalMainIdentifier;
 export 'package:flutter_portal/src/portal_target.dart'
     show PortalTarget, PortalNotFoundError;
