d6cd8aa3484c663ba1fcd86225cc41b803717325
fzyzcjy <ch271828n@outlook.com>
2022-09-18 15:30:45 +0800

    fix linter

--- lib/src/portal_target_theater.dart
+++ lib/src/portal_target_theater.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart'; // ignore: unnecessary_import
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
