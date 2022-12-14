6f3be7fd158c2afbef2ef3633f90005b2deda91c
Cedric Gestes <cedric@jots.ai>
2022-12-15 12:22:50 +1300

    export PortalLink, PortalLinkScope

--- lib/flutter_portal.dart
+++ lib/flutter_portal.dart
@@ -1,5 +1,7 @@
 export 'package:flutter_portal/src/anchor.dart'
     show Anchor, Aligned, AxisFlag, Filled;
 export 'package:flutter_portal/src/portal.dart' show Portal, PortalLabel;
+export 'package:flutter_portal/src/portal_link.dart'
+    show PortalLinkScope, PortalLink;
 export 'package:flutter_portal/src/portal_target.dart'
     show PortalTarget, PortalNotFoundError;
